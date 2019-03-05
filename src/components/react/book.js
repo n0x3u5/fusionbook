7eb2f486e268f0de880a25eed6ddf9a6558ea0b6
Md Owais <md.owais@fusioncharts.com>
2019-03-05 12:38:49 +0530

    structured base as book and sidebar as content

--- src/components/react/book.js
+++ src/components/react/book.js
@@ -1,20 +1,15 @@
 import React from 'react'
-import Story from './story'
 import { array } from 'prop-types'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-
-const createStory = (story = {}, idx) => (
-  <Story key={idx} storyName={story.name} chapters={story.chapters} />
-)
-const createStories = stories => stories.map(createStory)
+import Contents from './contents'
 
 const Book = ({ stories = [] }) => {
   return (
     <Container>
       <Row>
-        <Col sm={2}><ul>{createStories(stories)}</ul></Col>
+        <Col sm={2}><Contents stories={stories}/></Col>
         <Col sm={8}></Col>
       </Row>
     </Container>
