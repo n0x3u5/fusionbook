a0a1c49793fd4cffbc47ec13dcf40b79e7ec28d8
Md Owais <md.owais@fusioncharts.com>
2019-03-05 16:27:33 +0530

    corrected use of table of contents

--- src/client/components/react/book.js
+++ src/client/components/react/book.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { array } from 'prop-types'
 import { Grid } from 'semantic-ui-react'
+import Contents from './table-of-contents'
 
 const Book = ({ stories = [] }) => {
   return (
     <Grid columns={2}>
       <Grid.Column width={2}>
-        Hello
+        <Contents stories={stories}/>
       </Grid.Column>
       <Grid.Column width={14}>
-        Hello
       </Grid.Column>
     </Grid>
   )
