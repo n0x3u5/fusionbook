4d5bd00419aa4a9ff2eb480655aafc4ea1150563
siawo <md.owais1994@gmail.com>
2019-03-11 04:05:14 +0530

    #14: updated css and overrided setDimension api

--- stories/rectangle.js
+++ stories/rectangle.js
@@ -2,7 +2,7 @@ import SmartRenderer from '../../fc-core/src/component-interface/smart-renderer'
 
 class Rectangle extends SmartRenderer {
   __setDefaultConfig () {
-    this.config.fill = '#ff7200'
+    this.config.fill = '#511818'
     this.config.width = 300
     this.config.height = 300
     this.config.stroke = 'none'
@@ -10,6 +10,15 @@ class Rectangle extends SmartRenderer {
 
   configureAttributes (config = {}) {
     Object.assign(this.config, config)
+  }
+
+  setDimension (width, height) {
+    if (width < this.config.width) {
+      this.config.width = width
+    }
+    if (height < this.config.height) {
+      this.config.height = height
+    }
   }
 
   draw () {
