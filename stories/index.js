e99c88072cd53e88180914861c22f66dc2c78fa8
Nakshatra Mukhopadhyay <nakshatra.mukhopadhyay1994@gmail.com>
2019-03-11 15:24:37 +0530

    More chapters

--- stories/index.js
+++ stories/index.js
@@ -35,6 +35,20 @@ rectangleStory.addChapter(
   }
 )
 
+rectangleStory.addChapter(
+  'with a orange color',
+  {
+    content: story => story
+      .attachChild(Rectangle, 'rect')
+      .configure({ fill: 'orange' }),
+    meta: [
+      new Note('This is the rectangle as it appears when its width and height are 30px'),
+      new Config(),
+      new Event()
+    ]
+  }
+)
+
 const captionStory = new Story('Caption and Sub Caption')
 
 captionStory.addChapter(
