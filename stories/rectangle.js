bd711583b0fd88923388a20b0ba97ad6fdc96cde
Nakshatra Mukhopadhyay <nakshatra@fusioncharts.com>
2019-07-17 01:50:12 +0530

    Update rectangle story with correct usage of available width and height

--- stories/rectangle.js
+++ stories/rectangle.js
@@ -9,17 +9,25 @@ import Rectangle from '../components/rectangle.js'
 const rectangleStory = new Story('Rectangle')
 
 rectangleStory.addChapter('with defaults', {
-  content: story => story.attachChild(Rectangle, 'rect'),
+  content: story => {
+    const { availableWidth, availableHeight } = story.config
+    const rectangle = story.attachChild(Rectangle, 'rect')
+
+    rectangle.setDimension(availableWidth, availableHeight)
+  },
   meta: [
-    new Note('This is the rectangle as it appears by default'),
+    new Note('This is the rectangle as it appears by default, when its provided a width of 300 and a height of 300'),
     new Config(),
     new Event()
   ]
 })
 
 rectangleStory.addChapter('with a small size', {
-  content: story =>
-    story.attachChild(Rectangle, 'rect').configure({ width: 30, height: 30 }),
+  content: story => {
+    const rectangle = story.attachChild(Rectangle, 'rect')
+
+    rectangle.setDimension(30, 30)
+  },
   meta: [
     new Note(
       'This is the rectangle as it appears when its width and height are 30px'
@@ -30,11 +38,17 @@ rectangleStory.addChapter('with a small size', {
 })
 
 rectangleStory.addChapter('with a orange color', {
-  content: story =>
-    story.attachChild(Rectangle, 'rect').configure({ fill: 'orange' }),
+  content: story => {
+    const { availableWidth, availableHeight } = story.config
+    const rectangle = story.attachChild(Rectangle, 'rect')
+
+    rectangle.configure({ fill: 'orange' })
+
+    rectangle.setDimension(availableWidth, availableHeight)
+  },
   meta: [
     new Note(
-      'This is the rectangle as it appears when its width and height are 30px'
+      'This is the rectangle as it appears when its color is orange'
     ),
     new Config(),
     new Event()
