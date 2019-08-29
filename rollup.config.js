57994e85a1b46a3f2b97ba7c15bc326b5cd40973
Nakshatra Mukhopadhyay <nakshatra.mukhopadhyay1994@gmail.com>
2019-08-30 02:43:44 +0530

    WIP migration

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,46 @@
-import rollupTS from 'rollup-plugin-typescript2'
-import resolve from 'rollup-plugin-node-resolve'
+import path from 'path'
+
 import commonjs from 'rollup-plugin-commonjs'
-import html from 'rollup-plugin-fill-html'
+import copy from 'rollup-plugin-copy'
+import html from 'rollup-plugin-generate-html'
+import resolve from 'rollup-plugin-node-resolve'
 import replace from 'rollup-plugin-replace'
+import rollupTS from 'rollup-plugin-typescript2'
 import postcss from 'rollup-plugin-postcss'
 import postcssCopy from 'postcss-copy'
 import typescript from 'typescript'
 
+const NODE_ENV = process.env.NODE_ENV
+const IS_PROD = NODE_ENV === 'production'
+const NAME_MID_REACT = IS_PROD ? 'production.min' : 'development'
+const NAME_REACT = `react.${NAME_MID_REACT}.js`
+const NAME_REACT_DOM = `react-dom.${NAME_MID_REACT}.js`
+const PATH_INPUT_FILE = path.join('.', 'src', 'client', 'index.tsx')
+const PATH_OUT_DIR = path.join('.', 'dist')
+const PATH_OUT_VENDORS = path.join(PATH_OUT_DIR, 'vendors')
+const PATH_OUT_FILE = path.join(PATH_OUT_DIR, 'bundle.js')
+const PATH_SRC_HTML = path.join('.', 'src', 'client', 'index.html')
+const PATH_DEST_HTML = path.join(PATH_OUT_DIR, 'index.html')
+const PATH_SRC_REACT = path.join(
+  '.',
+  'node_modules',
+  'react',
+  'umd',
+  NAME_REACT
+)
+const PATH_SRC_REACT_DOM = path.join(
+  '.',
+  'node_modules',
+  'react-dom',
+  'umd',
+  NAME_REACT_DOM
+)
+
 export default {
-  input: './src/client/index.tsx',
+  input: PATH_INPUT_FILE,
   output: {
-    file: './dist/bundle.js',
+    file: PATH_OUT_FILE,
     format: 'iife',
-    name: 'customName',
     globals: {
       react: 'React',
       'react-dom': 'ReactDOM'
@@ -21,33 +49,35 @@ export default {
   external: ['react', 'react-dom'],
   plugins: [
     resolve(),
-    replace({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) }),
+    replace({ 'process.env.NODE_ENV': JSON.stringify(NODE_ENV) }),
     commonjs({
       namedExports: { 'react-is': ['isForwardRef'] }
     }),
     postcss({
       extract: true,
-      plugins: [ postcssCopy({ dest: 'dist' }) ]
+      plugins: [
+        postcssCopy({
+          template: 'assets/[hash].[ext]',
+          dest: PATH_OUT_DIR
+        })
+      ]
     }),
     rollupTS({
       typescript,
+      clean: true,
       tsconfigOverride: {
         compilerOptions: { jsx: 'react' }
       }
     }),
-    html({
-      template: './src/client/index.html',
-      externals: [
+    html({ filename: PATH_DEST_HTML, template: PATH_SRC_HTML }),
+    copy({
+      targets: [
         {
-          type: 'js',
-          file: './node_modules/react/umd/react.development.js',
-          pos: 'before'
+          src: PATH_SRC_REACT_DOM,
+          dest: PATH_OUT_VENDORS,
+          rename: 'react-dom.js'
         },
-        {
-          type: 'js',
-          file: './node_modules/react-dom/umd/react-dom.development.js',
-          pos: 'before'
-        }
+        { src: PATH_SRC_REACT, dest: PATH_OUT_VENDORS, rename: 'react.js' }
       ]
     })
   ],
